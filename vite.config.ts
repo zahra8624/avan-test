167948cb81be1dc00d9e4021f28247bcf5c2b71f
zahra8624 <sania.8624@gmail.com>
2024-06-27 14:40:13 -0700

    build(project):some other configs to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+//@ts-ignore
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      //@ts-ignore
+      "@app": path.resolve(__dirname, "./src"),
+    },
   },
 });
